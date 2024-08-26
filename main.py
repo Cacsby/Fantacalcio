c55b2a91324365a77cb5d287381478776f4f17e1
Cacsby <00noartist00@gmail.com>
2024-08-26 21:11:24 +0200

    implement part of logic

--- main.py
+++ main.py
@@ -16,6 +16,10 @@ def fantacalcio(self):
     team1 = Squadra("milan")
     team2 = Squadra("roma")
 
+    fantacalcio = []
+    fantacalcio.append(team1)
+    fantacalcio.append(team2)
+
 
     # Specifica il percorso del file .xlsx
     file_path = 'C:\\Users\\user\\Desktop\\Quotazioni_Fantacalcio_Stagione_2024_25.xlsx'
@@ -31,6 +35,8 @@ def fantacalcio(self):
     # Seleziona una colonna specifica
     # colonna_specifica = df['Nome']
     lista_giocatori = df.values.tolist()
+
+    #remove se si toglie prima riga file
     lista_giocatori.remove(lista_giocatori[0])
 
     while lista_giocatori:
@@ -38,16 +44,38 @@ def fantacalcio(self):
 
         giocatore_estratto = lista_giocatori[numero_random_lista_giocatori]
 
-        squadra_vincente_giocatore = "carico"
+        esito = "yes"
+        print(f"Giocatore estratto: {giocatore_estratto}")
 
-        if squadra_vincente_giocatore == "carico":
-            crea_giocatore_inserisci_in_squadra(self,giocatore_estratto, 200, team1, lista_giocatori)
+        while esito == "yes":
+
+            if giocatore_estratto not in lista_giocatori:
+                lista_giocatori.remove(giocatore_estratto)
+            print(f"Quale squadra si è aggiudicato {giocatore_estratto}?")
+            stampa_tutte_squadre(self, fantacalcio)
+
+            squadra_vincente_giocatore = int(input(""))
+
+            print(f"Quanto è stato pagato: {giocatore_estratto}")
+
+            prezzo_giocatore_estratto = int(input(""))
+
+            print(f"Devi modificare qualcosa?")
+
+            esito = input("yes/no")
 
 
-        elif squadra_vincente_giocatore == "cac":
-            cac.squadra.append(giocatore_estratto)
-            lista_giocatori.remove(giocatore_estratto)
+        # squadra_vincente_giocatore = "carico"
 
+        if squadra_vincente_giocatore == 0:
+            if is_max_portieri(self, team1) == False:
+                crea_giocatore_inserisci_in_squadra(self,giocatore_estratto, prezzo_giocatore_estratto, team1, lista_giocatori)
+
+
+        elif squadra_vincente_giocatore == 1:
+            if is_max_portieri(self, team2) == False:
+                crea_giocatore_inserisci_in_squadra(self, giocatore_estratto, prezzo_giocatore_estratto, team2,
+                                                lista_giocatori)
 
     print(df)
 
@@ -61,4 +89,17 @@ def aggiorna_crediti_rimanenti(squadra_vincente_giocatore: Squadra, prezzo:int):
     if(squadra_vincente_giocatore.crediti_totali > prezzo):
         squadra_vincente_giocatore.crediti_totali = squadra_vincente_giocatore.crediti_totali - prezzo
     else:
-        print("crediti insufficienti")
+        return print("crediti insufficienti")
+
+def stampa_tutte_squadre(self, fantacalcio: list):
+    count = 0
+    for squadra in fantacalcio:
+        print (f"Digitare {count} per la squadra {squadra.nome_squadra}. Crediti rimanenti: {squadra.crediti_totali}")
+        count = count + 1
+
+def is_max_portieri(self, squadra: Squadra):
+    if(len(squadra.squadra) > 2):
+        print("Numero massimo di portieri raggiunto")
+        return True
+    else
+        return False
